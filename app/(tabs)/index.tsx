96046aeeddb017b41000a102b9c4f8d41f89e972
Emre Ozturk <emreeozturk42@gmail.com>
2025-06-13 04:13:21 +0300

    Enhance app layout and navigation: Added a custom header with logo and user profile button in RootLayout, updated HomeScreen to use FlatList for item display, and removed unused components for cleaner code.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,72 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+
+const DATA = [...Array(24)].map((_, index) => ({
+  id: index.toString(),
+  title: (index + 1).toString(),
+}));
+
+const Item = ({ title }: { title: string }) => (
+  <TouchableOpacity
+    onPress={() => {
+      console.log('item pressed', title);
+    }}
+    style={styles.item}>
+    <Text>{title}</Text>
+  </TouchableOpacity>
+);
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <View style={styles.emptyArea}>
+        <Text style={styles.emptyAreaText}>
+          Welcome to Devember
+        </Text>
+      </View>
+      <FlatList
+        style={styles.listArea}
+        data={DATA}
+        renderItem={({ item }) => <Item title={item.title} />}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    backgroundColor: '#1D3D47',
+  },
+  item: {
+    backgroundColor: '#3f3f3f',
+    padding: 30,
+    borderRadius: 10,
+    borderWidth: .3,
+    borderColor: '#fff',
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
+    flex: 1,
+    aspectRatio: 1,
+    margin: 12,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  listArea: {
+    flex: 1,
+    backgroundColor: '#06232b',
+    padding: 16,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  emptyArea: {
+    height: 200,
+  },
+  emptyAreaText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+    marginTop: 100,
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
+import { Image } from 'expo-image';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -19,11 +21,54 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack >
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: true,
+            headerTitle: 'Devember Day 1',
+            headerLeft: () => (
+              <Image
+                source={require('../assets/images/react-logo.png')}
+                style={styles.reactLogo}
+              />
+            ),
+            headerTitleStyle: {
+              fontSize: 20,
+              fontWeight: 'bold',
+            },
+            headerRight: () => (
+              <TouchableOpacity
+                onPress={() => {
+                  console.log('user profile image pressed');
+                }}
+                style={styles.userProfileContainer}>
+                <Image
+                  source={require('../assets/images/react-logo.png')}
+                  style={styles.userProfileImage}
+                />
+              </TouchableOpacity>
+            ),
+          }}
+        />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  reactLogo: {
+    width: 80,
+    height: 80,
+  },
+  userProfileImage: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+  userProfileContainer: {
+    marginRight: 10,
+  },
+});
